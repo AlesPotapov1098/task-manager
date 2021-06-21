149d41efe4ad30351ef1c294b54e66a3d03f3157
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-06-21 22:44:24 +0700

    Переименовал файлы и форму

--- task-manager/task-manager/MainForm.Designer.cs
+++ task-manager/task-manager/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace task_manager
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,10 +29,17 @@ namespace task_manager
         /// </summary>
         private void InitializeComponent ()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Task Manager";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
